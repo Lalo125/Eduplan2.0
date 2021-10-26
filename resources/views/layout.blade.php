fafd16909fa23afff12c3333c689c52b6aae92d2
RodrigoAVV <rodrigo.vidal@virginiogomez.cl>
2021-10-26 12:51:55 -0300

    Se agrega layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,8 +3,8 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-		<title>Hello, world!</title>
+		<link href="{{url('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css')}}" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		<title>@yield('title')</title>
 	</head>
 	<body>
 		<!-- 
@@ -62,7 +62,34 @@
 			
 			
 			<!-- Inicio  contenido principal -->
-			<h1>Eduplan2.0</h1>
+			
+						<!-- Para mensajes de alerta -->
+			@if (session('warning'))
+				<div class="alert alert-warning">
+				{{ session('warning') }}
+				</div>
+			@endif
+			
+			<!-- Para mensajes de 'éxito -->
+			@if (session('success'))
+				<div class="alert alert-success">
+				{{ session('success') }}
+				</div>
+			@endif
+			
+			<!-- Para validación de formularios -->
+			@if ($errors->any())
+				<div class="alert alert-danger">
+					<ul>
+						@foreach ($errors->all() as $error)
+						<li>{{ $error }}</li>
+						@endforeach
+					</ul>
+				</div>
+			@endif
+			
+			@yield('content')
+			
 			<!-- Fin del contenido principal -->
 			
 			
@@ -122,7 +149,7 @@
 		Fin contenedor principal 
 		
 		-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="{{ url('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js') }}" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 	</body>
 </html>
